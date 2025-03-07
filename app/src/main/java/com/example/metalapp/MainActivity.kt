a50be04843ccbafbae79f63904b68a241ef5b884
katanawka <morozovvadim0711@gmail.com>
2025-03-07 03:27:00 +0200

    Fix Layout

--- app/src/main/java/com/example/metalapp/MainActivity.kt
+++ app/src/main/java/com/example/metalapp/MainActivity.kt
@@ -1,23 +1,18 @@
 package com.example.metalapp
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var metalAppNameTextView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        metalAppNameTextView = findViewById(R.id.MetalAppName)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.vertical)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
